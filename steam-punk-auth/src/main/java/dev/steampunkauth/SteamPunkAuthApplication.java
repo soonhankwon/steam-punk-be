3399090cf4684fa0f4743a51129df81c254be084
soonhankwon <soonable@naver.com>
2024-05-09 23:09:57 +0900

    feat: 인증 마이크로서비스

---
+++ steam-punk-auth/src/main/java/dev/steampunkauth/SteamPunkAuthApplication.java
@@ -0,0 +1,13 @@
+package dev.steampunkauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamPunkAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamPunkAuthApplication.class, args);
+	}
+
+}
---
+++ steam-punk-auth/src/test/java/dev/steampunkauth/SteamPunkAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.steampunkauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamPunkAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
