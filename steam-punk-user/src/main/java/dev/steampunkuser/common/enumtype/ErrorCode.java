2e3456cb795b6cd5829fc076b6b5df3e093d525a
soonhankwon <soonable@naver.com>
2024-05-09 21:58:13 +0900

    feat: 유저아이디(PK) 미존재 에러코드 추가

--- steam-punk-user/src/main/java/dev/steampunkuser/common/enumtype/ErrorCode.java
+++ steam-punk-user/src/main/java/dev/steampunkuser/common/enumtype/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     EXISTS_DUPLICATED_EMAIL(HttpStatus.CONFLICT, "중복된 이메일이 존재합니다."),
     DECRYPTION_FAILED(HttpStatus.BAD_REQUEST, "복호화가 실패했습니다."),
     ENCRYPTION_FAILED(HttpStatus.BAD_REQUEST, "암호화가 실패했습니다."),
-    CRYPTOGRAPHY_FAILED(HttpStatus.BAD_REQUEST, "암복호화가 실패했습니다.");
+    CRYPTOGRAPHY_FAILED(HttpStatus.BAD_REQUEST, "암복호화가 실패했습니다."),
+    NOT_EXISTS_USER_ID(HttpStatus.BAD_REQUEST, "유저아이디가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
