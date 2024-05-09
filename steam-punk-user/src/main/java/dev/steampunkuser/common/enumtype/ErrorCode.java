2c47e8278399408eab2c03be23b5af18fc101ae3
soonhankwon <soonable@naver.com>
2024-05-09 20:50:35 +0900

    feat: 암호화관련 에러코드 추가

--- steam-punk-user/src/main/java/dev/steampunkuser/common/enumtype/ErrorCode.java
+++ steam-punk-user/src/main/java/dev/steampunkuser/common/enumtype/ErrorCode.java
@@ -7,8 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    
-    EXISTS_DUPLICATED_EMAIL(HttpStatus.CONFLICT, "중복된 이메일이 존재합니다.");
+
+    EXISTS_DUPLICATED_EMAIL(HttpStatus.CONFLICT, "중복된 이메일이 존재합니다."),
+    DECRYPTION_FAILED(HttpStatus.BAD_REQUEST, "복호화가 실패했습니다."),
+    ENCRYPTION_FAILED(HttpStatus.BAD_REQUEST, "암호화가 실패했습니다."),
+    CRYPTOGRAPHY_FAILED(HttpStatus.BAD_REQUEST, "암복호화가 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
