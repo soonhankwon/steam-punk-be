61e7d7223257bbdaf685bba6317b8cdf91798276
soonhankwon <soonable@naver.com>
2024-05-09 19:30:54 +0900

    feat: User 레포지토리

---
+++ steam-punk-user/src/main/java/dev/steampunkuser/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.steampunkuser.repository;
+
+import dev.steampunkuser.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
