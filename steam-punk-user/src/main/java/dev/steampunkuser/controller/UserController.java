c14e6390159509dad90573673c5cd03dd5b7157e
soonhankwon <soonable@naver.com>
2024-05-09 21:59:12 +0900

    feat: 전화번호 업데이트 API(PATCH)

--- steam-punk-user/src/main/java/dev/steampunkuser/controller/UserController.java
+++ steam-punk-user/src/main/java/dev/steampunkuser/controller/UserController.java
@@ -1,12 +1,15 @@
 package dev.steampunkuser.controller;
 
 import dev.steampunkuser.dto.request.UserAddRequest;
+import dev.steampunkuser.dto.request.UserPhoneNumberUpdateRequest;
 import dev.steampunkuser.dto.response.UserAddResponse;
+import dev.steampunkuser.dto.response.UserPhoneNumberUpdateResponse;
 import dev.steampunkuser.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +27,12 @@ public class UserController {
         UserAddResponse res = userService.addUser(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(res);
+    }
+
+    @PatchMapping("/phone")
+    public ResponseEntity<UserPhoneNumberUpdateResponse> updatePhoneNumber(
+            @RequestBody UserPhoneNumberUpdateRequest request) {
+        UserPhoneNumberUpdateResponse res = userService.updatePhoneNumber(request);
+        return ResponseEntity.ok(res);
     }
 }
