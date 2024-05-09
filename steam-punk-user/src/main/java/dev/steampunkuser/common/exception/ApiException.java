94fb9459f571d2a5550e802c02497b1561948dee
soonhankwon <soonable@naver.com>
2024-05-09 20:49:10 +0900

    feat: 불필요 멤버변수 삭제(ErrorCode에 포함)

--- steam-punk-user/src/main/java/dev/steampunkuser/common/exception/ApiException.java
+++ steam-punk-user/src/main/java/dev/steampunkuser/common/exception/ApiException.java
@@ -3,12 +3,10 @@ package dev.steampunkuser.common.exception;
 import dev.steampunkuser.common.enumtype.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public class ApiException extends RuntimeException {
 
-    private final HttpStatus httpStatus;
     private final ErrorCode errorCode;
 }
