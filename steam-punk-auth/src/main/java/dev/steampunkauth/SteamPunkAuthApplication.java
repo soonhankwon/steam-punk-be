1b191ed1f2378149141b094b5e008099b91d29b5
soonhankwon <soonable@naver.com>
2024-05-09 23:58:36 +0900

    feat: 인증서비스 시큐리티 OFF

--- steam-punk-auth/src/main/java/dev/steampunkauth/SteamPunkAuthApplication.java
+++ steam-punk-auth/src/main/java/dev/steampunkauth/SteamPunkAuthApplication.java
@@ -2,12 +2,13 @@ package dev.steampunkauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SteamPunkAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SteamPunkAuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SteamPunkAuthApplication.class, args);
+    }
 
 }
