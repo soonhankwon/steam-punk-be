fc5360694d4fca77777197cc25cb0ce00a5793a4
soonhankwon <soonable@naver.com>
2024-05-09 18:32:33 +0900

    init

---
+++ steam-punk-user/src/main/java/dev/steampunkuser/SteamPunkUserApplication.java
@@ -0,0 +1,13 @@
+package dev.steampunkuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamPunkUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamPunkUserApplication.class, args);
+    }
+
+}
