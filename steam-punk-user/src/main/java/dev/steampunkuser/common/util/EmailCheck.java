db593b2fdbdf817ed78b25e9a50fa2a3a8141309
soonhankwon <soonable@naver.com>
2024-05-09 20:21:27 +0900

    feat: EmailCheck 애노테이션

---
+++ steam-punk-user/src/main/java/dev/steampunkuser/common/util/EmailCheck.java
@@ -0,0 +1,18 @@
+package dev.steampunkuser.common.util;
+
+import jakarta.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EmailValidator.class)
+public @interface EmailCheck {
+    String message() default "이메일이 유효하지 않습니다.";
+
+    Class[] groups() default {};
+
+    Class[] payload() default {};
+}
