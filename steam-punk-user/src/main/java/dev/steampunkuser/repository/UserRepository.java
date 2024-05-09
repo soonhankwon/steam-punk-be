b09009eb7724146f68484f356e75b7505a35754d
soonhankwon <soonable@naver.com>
2024-05-09 23:57:04 +0900

    feat: 유저 이메일로 조회 쿼리

--- steam-punk-user/src/main/java/dev/steampunkuser/repository/UserRepository.java
+++ steam-punk-user/src/main/java/dev/steampunkuser/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package dev.steampunkuser.repository;
 
 import dev.steampunkuser.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
