8ae417cb0de83b4359c52ba5125be6446df1e507
soonhankwon <soonable@naver.com>
2024-05-10 00:27:37 +0900

    feat: 상품(게임) 레포지토리

---
+++ steam-punk-product/src/main/java/dev/steampunkproduct/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package dev.steampunkproduct.repository;
+
+import dev.steampunkproduct.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
