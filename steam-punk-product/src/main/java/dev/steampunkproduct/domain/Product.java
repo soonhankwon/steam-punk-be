1226779bb7467c388405d2f6d6b98b132f3723a7
soonhankwon <soonable@naver.com>
2024-05-10 00:27:07 +0900

    feat: 상품 엔티티 모델링

---
+++ steam-punk-product/src/main/java/dev/steampunkproduct/domain/Product.java
@@ -0,0 +1,29 @@
+package dev.steampunkproduct.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    private String name;
+
+    private Long price;
+
+    private String description;
+}
