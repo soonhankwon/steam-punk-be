91ff7437a762425106d28a87706f8ce2605b0a8e
soonhankwon <soonable@naver.com>
2024-05-09 23:59:37 +0900

    feat: 로그인 비즈니스 로직

---
+++ steam-punk-auth/src/main/java/dev/steampunkauth/service/AuthService.java
@@ -0,0 +1,49 @@
+package dev.steampunkauth.service;
+
+import dev.steampunkauth.common.util.JwtProvider;
+import dev.steampunkauth.domain.UserInfo;
+import dev.steampunkauth.dto.request.AuthLoginRequest;
+import dev.steampunkauth.dto.response.AuthLoginResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AuthService {
+
+    private final JwtProvider jwtProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthLoginResponse login(AuthLoginRequest request, HttpServletResponse httpServletResponse) {
+        String defaultAccessToken = jwtProvider.createDefaultToken(true);
+        UserInfo userInfo = getUserInfo(request, defaultAccessToken);
+        
+        if (!userInfo.isValid()) {
+            throw new IllegalArgumentException("invalid access");
+        }
+
+        userInfo.validatePassword(request.password(), passwordEncoder::matches);
+        Long userId = userInfo.userId();
+        String accessToken = jwtProvider.createJwt(userId, true);
+        httpServletResponse.setHeader(HttpHeaders.AUTHORIZATION, accessToken);
+
+        String refreshToken = jwtProvider.createJwt(userId, false);
+        return AuthLoginResponse.of(userInfo, refreshToken);
+    }
+
+    private UserInfo getUserInfo(AuthLoginRequest request, String accessToken) {
+        return WebClient.create()
+                .get()
+                .uri("http://localhost:8080/api/v1/users/" + request.email())
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(accessToken))
+                .retrieve()
+                .bodyToMono(UserInfo.class)
+                .block();
+    }
+}
