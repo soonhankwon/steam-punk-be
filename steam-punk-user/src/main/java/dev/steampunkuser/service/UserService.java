f0c4fdbdc458a2c827a43d5c31499a351a10b6d9
soonhankwon <soonable@naver.com>
2024-05-09 21:59:52 +0900

    feat: 전화번호 업데이트 비즈니스 로직

--- steam-punk-user/src/main/java/dev/steampunkuser/service/UserService.java
+++ steam-punk-user/src/main/java/dev/steampunkuser/service/UserService.java
@@ -1,8 +1,12 @@
 package dev.steampunkuser.service;
 
+import dev.steampunkuser.common.enumtype.ErrorCode;
+import dev.steampunkuser.common.exception.ApiException;
 import dev.steampunkuser.domain.User;
 import dev.steampunkuser.dto.request.UserAddRequest;
+import dev.steampunkuser.dto.request.UserPhoneNumberUpdateRequest;
 import dev.steampunkuser.dto.response.UserAddResponse;
+import dev.steampunkuser.dto.response.UserPhoneNumberUpdateResponse;
 import dev.steampunkuser.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,5 +25,14 @@ public class UserService {
         User user = User.from(request, passwordEncoder::encode);
         user = userRepository.save(user);
         return UserAddResponse.from(user);
+    }
+
+    @Transactional
+    public UserPhoneNumberUpdateResponse updatePhoneNumber(UserPhoneNumberUpdateRequest request) {
+        User user = userRepository.findById(request.userId())
+                .orElseThrow(() -> new ApiException(ErrorCode.NOT_EXISTS_USER_ID));
+
+        user.updatePhoneNumber(request);
+        return UserPhoneNumberUpdateResponse.from(user);
     }
 }
