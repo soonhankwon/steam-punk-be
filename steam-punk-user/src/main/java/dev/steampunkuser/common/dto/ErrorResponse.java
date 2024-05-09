e73b0ace6f463a45d8fb2885ba4ff9d1ff75d1b6
soonhankwon <soonable@naver.com>
2024-05-09 20:50:53 +0900

    feat: 에러코드에서 HttpStatus 조회 및 사용

--- steam-punk-user/src/main/java/dev/steampunkuser/common/dto/ErrorResponse.java
+++ steam-punk-user/src/main/java/dev/steampunkuser/common/dto/ErrorResponse.java
@@ -1,5 +1,6 @@
 package dev.steampunkuser.common.dto;
 
+import dev.steampunkuser.common.enumtype.ErrorCode;
 import dev.steampunkuser.common.exception.ApiException;
 
 public record ErrorResponse(
@@ -7,9 +8,10 @@ public record ErrorResponse(
         String message
 ) {
     public static ErrorResponse from(ApiException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
         return new ErrorResponse(
-                exception.getHttpStatus().value(),
-                exception.getErrorCode().getMessage()
+                errorCode.getHttpStatus().value(),
+                errorCode.getMessage()
         );
     }
 }
