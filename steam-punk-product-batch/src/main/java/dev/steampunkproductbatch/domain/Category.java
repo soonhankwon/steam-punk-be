529bbb6f5d0f19ba5a839a4cc5ede2e692ff2e35
soonhankwon <soonable@naver.com>
2024-05-11 00:10:27 +0900

    feat: 카테고리 엔티티 & 레포지토리

---
+++ steam-punk-product-batch/src/main/java/dev/steampunkproductbatch/domain/Category.java
@@ -0,0 +1,30 @@
+package dev.steampunkproductbatch.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+}
