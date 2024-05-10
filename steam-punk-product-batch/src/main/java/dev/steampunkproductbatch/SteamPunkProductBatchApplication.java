b8e766290bdf51fe3699aca09b71fba3def351bc
soonhankwon <soonable@naver.com>
2024-05-10 14:10:41 +0900

    feat: 상품 배치(batch) 모듈

---
+++ steam-punk-product-batch/src/main/java/dev/steampunkproductbatch/SteamPunkProductBatchApplication.java
@@ -0,0 +1,13 @@
+package dev.steampunkproductbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamPunkProductBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamPunkProductBatchApplication.class, args);
+	}
+
+}
---
+++ steam-punk-product-batch/src/test/java/dev/steampunkproductbatch/SteamPunkProductBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.steampunkproductbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamPunkProductBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
