14b9f851daf48efdf6e1a5131f1924fd74078920
soonhankwon <soonable@naver.com>
2024-05-12 21:35:57 +0900

    feat: OneToMany 애노테이션 추가

--- steam-punk-product-batch/src/main/java/dev/steampunkproductbatch/domain/Product.java
+++ steam-punk-product-batch/src/main/java/dev/steampunkproductbatch/domain/Product.java
@@ -1,11 +1,15 @@
 package dev.steampunkproductbatch.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,6 +32,9 @@ public class Product {
     private String headerImage;
     private String webSite;
     private String developer;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private final List<ProductCategory> productCategories = new ArrayList<>();
 
     public Product(String name, Double price, String shortDescription, String headerImage, String webSite,
                    String developer) {
