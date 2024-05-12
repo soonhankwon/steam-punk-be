e3471f0f7b3a135e8ed04498a2dc99b69481dac3
soonhankwon <soonable@naver.com>
2024-05-12 21:35:26 +0900

    feat: Json 파싱 커스텀 ItemReader

---
+++ steam-punk-product-batch/src/main/java/dev/steampunkproductbatch/reader/JsonProductItemReader.java
@@ -0,0 +1,58 @@
+package dev.steampunkproductbatch.reader;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.steampunkproductbatch.domain.Product;
+import dev.steampunkproductbatch.domain.ProductCategory;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+public class JsonProductItemReader implements ItemReader<Product> {
+
+    private final Iterator<JsonNode> productIterator;
+    private final Map<String, Long> internalCacheMap;
+
+    public JsonProductItemReader(String filePath, Map<String, Long> cacheMap) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(new File(filePath));
+        this.productIterator = rootNode.elements();
+        this.internalCacheMap = cacheMap;
+    }
+
+    @Override
+    public Product read() throws UnexpectedInputException, ParseException, NonTransientResourceException {
+        if (productIterator != null && productIterator.hasNext()) {
+            JsonNode node = productIterator.next();
+            String name = node.get("name").asText();
+            double price = node.get("price").asDouble();
+            String shortDescription = node.get("short_description").asText();
+            String headerImage = node.get("header_image").asText();
+            String webSite = node.get("website").asText();
+            JsonNode developersJsonNode = node.get("developers");
+            String developer;
+            if (developersJsonNode == null || developersJsonNode.isEmpty()) {
+                developer = "Anonymous";
+            } else {
+                developer = developersJsonNode.get(0).asText();
+            }
+            Product product = new Product(name, price, shortDescription, headerImage, webSite, developer);
+            JsonNode genresJsonNode = node.get("genres");
+            if (genresJsonNode != null && !genresJsonNode.isEmpty()) {
+                Iterator<JsonNode> iterator = genresJsonNode.iterator();
+                iterator.forEachRemaining(g -> {
+                    String categoryName = g.asText();
+                    Long categoryId = internalCacheMap.get(categoryName);
+                    product.getProductCategories().add(new ProductCategory(product, categoryId));
+                });
+            }
+            return product;
+        }
+        return null;
+    }
+}
