6de3d61dd21e77294bc79e419f6d6a91646882cd
soonhankwon <soonable@naver.com>
2024-05-13 17:07:25 +0900

    feat: 위시리스트 추가, 조회 API

---
+++ steam-punk-order/src/main/java/dev/steampunkorder/controller/WishListController.java
@@ -0,0 +1,34 @@
+package dev.steampunkorder.controller;
+
+import dev.steampunkorder.dto.request.WishListAddRequest;
+import dev.steampunkorder.dto.response.WishListAddResponse;
+import dev.steampunkorder.dto.response.WishListGetResponse;
+import dev.steampunkorder.service.WishListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/wishlists")
+public class WishListController {
+
+    private final WishListService wishListService;
+
+    @PostMapping
+    public ResponseEntity<WishListAddResponse> addWishList(@RequestBody WishListAddRequest request) {
+        WishListAddResponse res = wishListService.addWishList(request);
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<WishListGetResponse> getWishList(@PathVariable Long userId) {
+        WishListGetResponse res = wishListService.getWishList(userId);
+        return ResponseEntity.ok(res);
+    }
+}
