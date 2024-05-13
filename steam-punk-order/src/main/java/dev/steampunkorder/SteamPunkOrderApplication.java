ad0c347d7d5011ed51448c19bd7f4aa351626b78
soonhankwon <soonable@naver.com>
2024-05-13 15:48:09 +0900

    feat: Order 마이크로서비스 모듈 추가

---
+++ steam-punk-order/src/main/java/dev/steampunkorder/SteamPunkOrderApplication.java
@@ -0,0 +1,13 @@
+package dev.steampunkorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamPunkOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamPunkOrderApplication.class, args);
+    }
+
+}
---
+++ steam-punk-order/src/test/java/dev/steampunkorder/SteamPunkOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.steampunkorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamPunkOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
