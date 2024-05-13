4a27f3f63f2b1c6242c2de48ca87fe299f72458c
soonhankwon <soonable@naver.com>
2024-05-13 17:27:50 +0900

    feat: 위시리스트에 상품 ID 미존재 에러코드

--- steam-punk-order/src/main/java/dev/steampunkorder/common/enumtype/ErrorCode.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/common/enumtype/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     NOT_EXIST_PRODUCT_ID(HttpStatus.BAD_REQUEST, "상품 ID가 존재하지 않습니다."),
-    EXISTS_PRODUCT_ID_IN_WISHLIST(HttpStatus.BAD_REQUEST, "상품 ID가 위시리스트에 존재합니다.");
+    EXISTS_PRODUCT_ID_IN_WISHLIST(HttpStatus.BAD_REQUEST, "상품 ID가 위시리스트에 존재합니다."),
+    NOT_EXIST_PRODUCT_ID_BY_USER(HttpStatus.BAD_REQUEST, "유저 위시리스트에 상품 ID가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
