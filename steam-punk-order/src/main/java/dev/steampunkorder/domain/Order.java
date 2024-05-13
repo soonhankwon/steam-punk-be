0277fa7c34ec616183a0825682aeaac1824af83f
soonhankwon <soonable@naver.com>
2024-05-13 17:09:49 +0900

    feat: 주문 엔티티 정의

---
+++ steam-punk-order/src/main/java/dev/steampunkorder/domain/Order.java
@@ -0,0 +1,23 @@
+package dev.steampunkorder.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+}
