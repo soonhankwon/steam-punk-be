daddeff546d4f3725df9ab10b0e9b253516b6f78
soonhankwon <soonable@naver.com>
2024-05-13 15:44:05 +0900

    feat: 상품조회 및 상품목록조회(페이징) API

--- steam-punk-product/src/main/java/dev/steampunkproduct/controller/ProductController.java
+++ steam-punk-product/src/main/java/dev/steampunkproduct/controller/ProductController.java
@@ -2,11 +2,13 @@ package dev.steampunkproduct.controller;
 
 import dev.steampunkproduct.dto.response.ProductGetResponse;
 import dev.steampunkproduct.service.ProductService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -19,6 +21,12 @@ public class ProductController {
     @GetMapping("{productId}")
     public ResponseEntity<ProductGetResponse> getProduct(@PathVariable Long productId) {
         ProductGetResponse res = productService.getProduct(productId);
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductGetResponse>> getProducts(@RequestParam(value = "page_number") int pageNumber) {
+        List<ProductGetResponse> res = productService.getProducts(pageNumber);
         return ResponseEntity.ok(res);
     }
 }
