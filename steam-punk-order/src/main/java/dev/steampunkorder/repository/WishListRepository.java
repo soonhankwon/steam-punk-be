929500bda3168ac071c52f1828f096967a68695a
soonhankwon <soonable@naver.com>
2024-05-13 17:28:38 +0900

    feat: 유저Id, 상품Id로 위시리스트 조회

--- steam-punk-order/src/main/java/dev/steampunkorder/repository/WishListRepository.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/repository/WishListRepository.java
@@ -2,6 +2,7 @@ package dev.steampunkorder.repository;
 
 import dev.steampunkorder.domain.WishList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ public interface WishListRepository extends JpaRepository<WishList, Long> {
     List<WishList> findAllByUserId(Long userId);
 
     boolean existsByUserIdAndProductId(Long userId, Long productId);
+
+    Optional<WishList> findByUserIdAndProductId(Long userId, Long productId);
 }
