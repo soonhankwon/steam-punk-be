858f8da1d1c92863405a9b3735b3af372605fd5e
soonhankwon <soonable@naver.com>
2024-05-13 15:44:23 +0900

    feat: 상품조회 응답 레코드

--- steam-punk-product/src/main/java/dev/steampunkproduct/dto/response/ProductGetResponse.java
+++ steam-punk-product/src/main/java/dev/steampunkproduct/dto/response/ProductGetResponse.java
@@ -5,11 +5,26 @@ import dev.steampunkproduct.domain.Product;
 
 public record ProductGetResponse(
         @JsonProperty("product_id")
-        Long productId
+        Long productId,
+        String name,
+        Long price,
+        String description,
+        @JsonProperty("header_image")
+        String headerImage,
+        @JsonProperty("web_site")
+        String webSite,
+        String developer
+
 ) {
     public static ProductGetResponse from(Product product) {
         return new ProductGetResponse(
-                product.getId()
+                product.getId(),
+                product.getName(),
+                Math.round(product.getPrice() * 1300),
+                product.getShortDescription(),
+                product.getHeaderImage(),
+                product.getWebSite(),
+                product.getDeveloper()
         );
     }
 }
