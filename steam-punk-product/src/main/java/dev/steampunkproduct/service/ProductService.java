9ae0ad69ce686c65f1d33371f2aa88bded39baf4
soonhankwon <soonable@naver.com>
2024-05-13 15:44:39 +0900

    feat: 상품목록조회(페이징) 비즈니스 로직

--- steam-punk-product/src/main/java/dev/steampunkproduct/service/ProductService.java
+++ steam-punk-product/src/main/java/dev/steampunkproduct/service/ProductService.java
@@ -3,7 +3,10 @@ package dev.steampunkproduct.service;
 import dev.steampunkproduct.domain.Product;
 import dev.steampunkproduct.dto.response.ProductGetResponse;
 import dev.steampunkproduct.repository.ProductRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,11 +15,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private static final int PAGE_SIZE = 10;
 
     @Transactional(readOnly = true)
     public ProductGetResponse getProduct(Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new IllegalArgumentException("no product Id"));
         return ProductGetResponse.from(product);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductGetResponse> getProducts(int pageNumber) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, PAGE_SIZE);
+        return productRepository.findAll(pageRequest)
+                .stream()
+                .map(ProductGetResponse::from)
+                .collect(Collectors.toList());
     }
 }
