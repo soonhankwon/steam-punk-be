7a892367dd3d00dc61c3d0f2549fbe7923d6fd70
soonhankwon <soonable@naver.com>
2024-05-13 17:27:58 +0900

    feat: 위시리스트에 상품삭제 API

--- steam-punk-order/src/main/java/dev/steampunkorder/controller/WishListController.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/controller/WishListController.java
@@ -1,11 +1,14 @@
 package dev.steampunkorder.controller;
 
 import dev.steampunkorder.dto.request.WishListAddRequest;
+import dev.steampunkorder.dto.request.WishListDeleteRequest;
 import dev.steampunkorder.dto.response.WishListAddResponse;
+import dev.steampunkorder.dto.response.WishListDeleteResponse;
 import dev.steampunkorder.dto.response.WishListGetResponse;
 import dev.steampunkorder.service.WishListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +32,12 @@ public class WishListController {
     @GetMapping("/{userId}")
     public ResponseEntity<WishListGetResponse> getWishList(@PathVariable Long userId) {
         WishListGetResponse res = wishListService.getWishList(userId);
+        return ResponseEntity.ok(res);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<WishListDeleteResponse> deleteWishList(@RequestBody WishListDeleteRequest request) {
+        WishListDeleteResponse res = wishListService.deleteWishList(request);
         return ResponseEntity.ok(res);
     }
 }
