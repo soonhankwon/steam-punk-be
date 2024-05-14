e2d7dfc86c15d31481fa0621e3a99db04a75153e
soonhankwon <soonable@naver.com>
2024-05-14 17:43:25 +0900

    feat: 주문ID와 유저ID로 주문조회(유저의 주문)

--- steam-punk-order/src/main/java/dev/steampunkorder/repository/OrderRepository.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package dev.steampunkorder.repository;
 
 import dev.steampunkorder.domain.Order;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByIdAndUserId(Long orderId, Long userId);
 }
