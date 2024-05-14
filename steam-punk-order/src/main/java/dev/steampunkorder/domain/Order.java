c928b8e83287432e296434e25e8029a418558adf
soonhankwon <soonable@naver.com>
2024-05-14 15:30:29 +0900

    feat: 주문 엔티티 정의

--- steam-punk-order/src/main/java/dev/steampunkorder/domain/Order.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/domain/Order.java
@@ -1,7 +1,11 @@
 package dev.steampunkorder.domain;
 
+import dev.steampunkorder.common.entity.BaseTimeEntity;
+import dev.steampunkorder.enumtype.OrderState;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,11 +17,29 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "order")
-public class Order {
+@Table(name = "`order`")
+public class Order extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
     private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private OrderState orderState;
+
+    private Order(Long userId, OrderState orderState) {
+        this.userId = userId;
+        this.orderState = orderState;
+    }
+
+    public static Order ofPendingOrder(Long userId) {
+        return new Order(
+                userId,
+                OrderState.ORDER_PENDING
+        );
+    }
 }
