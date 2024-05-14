4b7c0d7520a07a8a69b9d5d83f9df6485416e2d4
soonhankwon <soonable@naver.com>
2024-05-14 16:33:29 +0900

    feat: 포인트 최소금액 에러코드 추가

--- steam-punk-user/src/main/java/dev/steampunkuser/common/enumtype/ErrorCode.java
+++ steam-punk-user/src/main/java/dev/steampunkuser/common/enumtype/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     DECRYPTION_FAILED(HttpStatus.BAD_REQUEST, "복호화가 실패했습니다."),
     ENCRYPTION_FAILED(HttpStatus.BAD_REQUEST, "암호화가 실패했습니다."),
     CRYPTOGRAPHY_FAILED(HttpStatus.BAD_REQUEST, "암복호화가 실패했습니다."),
-    NOT_EXISTS_USER_ID(HttpStatus.BAD_REQUEST, "유저아이디가 존재하지 않습니다.");
+    NOT_EXISTS_USER_ID(HttpStatus.BAD_REQUEST, "유저아이디가 존재하지 않습니다."),
+    TOO_LOW_POINT(HttpStatus.BAD_REQUEST, "포인트는 0이상이어야 합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
