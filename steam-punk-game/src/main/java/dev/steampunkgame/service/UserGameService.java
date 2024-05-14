beea9d2adcdd382033686584740ff4d819ac7f8c
soonhankwon <soonable@naver.com>
2024-05-14 19:00:57 +0900

    feat: 유저게임이력 추가 및 상태반영 비즈니스 로직

---
+++ steam-punk-game/src/main/java/dev/steampunkgame/service/UserGameService.java
@@ -0,0 +1,50 @@
+package dev.steampunkgame.service;
+
+import dev.steampunkgame.common.enumtype.ErrorCode;
+import dev.steampunkgame.common.exception.ApiException;
+import dev.steampunkgame.domain.UserGameHistory;
+import dev.steampunkgame.dto.request.UserGameAddRequest;
+import dev.steampunkgame.dto.request.UserGamePlayRequest;
+import dev.steampunkgame.dto.response.UserGameAddResponse;
+import dev.steampunkgame.dto.response.UserGamePlayResponse;
+import dev.steampunkgame.repository.UserGameHistoryRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserGameService {
+
+    private final UserGameHistoryRepository userGameHistoryRepository;
+
+    // 유저의 게임 주문 결제 후 호출될 비즈니스 로직: 게임을 플레이하지 않은 기본 이력으로 만들어짐
+    @Transactional
+    public List<UserGameAddResponse> addUserGame(UserGameAddRequest request) {
+        List<UserGameHistory> userGameHistories = new ArrayList<>();
+        request.productIds().forEach(id -> {
+            if (userGameHistoryRepository.existsByUserIdAndProductId(request.userId(), id)) {
+                throw new ApiException(ErrorCode.EXISTS_USER_GAME_HISTORY);
+            }
+            UserGameHistory userGameHistory = UserGameHistory.ofNotPlayedState(id, request);
+            userGameHistories.add(userGameHistory);
+        });
+        userGameHistoryRepository.saveAll(userGameHistories);
+        return userGameHistories.stream()
+                .map(UserGameAddResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public UserGamePlayResponse playGame(UserGamePlayRequest request) {
+        UserGameHistory userGameHistory = userGameHistoryRepository.findByProductIdAndUserId(request.productId(),
+                        request.userId())
+                .orElseThrow(() -> new ApiException(ErrorCode.NOT_EXISTS_USER_GAME_HISTORY));
+
+        userGameHistory.played();
+        return UserGamePlayResponse.from(userGameHistory);
+    }
+}
