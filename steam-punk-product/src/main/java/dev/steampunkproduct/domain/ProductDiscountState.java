cc304804ddff9dc0fb2202a1168781ea29c62476
soonhankwon <soonable@naver.com>
2024-05-15 00:10:40 +0900

    feat: 할인상태 Enum 추가

---
+++ steam-punk-product/src/main/java/dev/steampunkproduct/domain/ProductDiscountState.java
@@ -0,0 +1,16 @@
+package dev.steampunkproduct.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ProductDiscountState {
+
+    REGULAR(1.0),
+    SEVENTY_FIVE_PERCENT_SALE(0.75),
+    HALF_SALE(0.5),
+    TWENTY_FIVE_PERCENT_SALE(0.25);
+
+    private final double saleRate;
+}
