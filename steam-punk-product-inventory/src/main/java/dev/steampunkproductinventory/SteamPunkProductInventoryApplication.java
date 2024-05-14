b18be86385b337453675f5e1fc1baf806424e065
soonhankwon <soonable@naver.com>
2024-05-15 00:17:21 +0900

    feat: 상품(예약주문)재고관리 마이크로서비스 모듈 추가

---
+++ steam-punk-product-inventory/src/main/java/dev/steampunkproductinventory/SteamPunkProductInventoryApplication.java
@@ -0,0 +1,13 @@
+package dev.steampunkproductinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamPunkProductInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamPunkProductInventoryApplication.class, args);
+    }
+
+}
---
+++ steam-punk-product-inventory/src/test/java/dev/steampunkproductinventory/SteamPunkProductInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.steampunkproductinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamPunkProductInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
