b7748556759a40538dd6c474423538fef22ec21b
soonhankwon <soonable@naver.com>
2024-05-14 15:29:30 +0900

    feat: 결제 레포지토리

---
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package dev.steampunkpayment.repository;
+
+import dev.steampunkpayment.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
