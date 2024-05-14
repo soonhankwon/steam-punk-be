0b953c8122f1e2bcabf8b8a69c84097d408da6c1
soonhankwon <soonable@naver.com>
2024-05-14 17:47:01 +0900

    feat: 불필요 응답 필드 삭제

--- steam-punk-user/src/main/java/dev/steampunkuser/dto/response/UserPointUpdateResponse.java
+++ steam-punk-user/src/main/java/dev/steampunkuser/dto/response/UserPointUpdateResponse.java
@@ -5,15 +5,12 @@ import dev.steampunkuser.domain.User;
 import java.time.LocalDateTime;
 
 public record UserPointUpdateResponse(
-        @JsonProperty("is_updated")
-        boolean isUpdated,
         Long point,
         @JsonProperty("updated_at")
         LocalDateTime updatedAt
 ) {
     public static UserPointUpdateResponse from(User user) {
         return new UserPointUpdateResponse(
-                true,
                 user.getPoint(),
                 LocalDateTime.now()
         );
