82c0168ed4b0684979a5b0507f87d6878ad73cff
soonhankwon <soonable@naver.com>
2024-05-14 15:32:14 +0900

    feat: 주문 레포지토리

---
+++ steam-punk-order/src/main/java/dev/steampunkorder/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package dev.steampunkorder.repository;
+
+import dev.steampunkorder.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
