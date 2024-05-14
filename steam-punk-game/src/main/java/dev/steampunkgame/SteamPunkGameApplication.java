8478dec8e28a2cd0c544bb44561c9800dac90389
soonhankwon <soonable@naver.com>
2024-05-14 18:55:31 +0900

    feat: 게임 마이크로서비스(유저게임이력) 모듈

---
+++ steam-punk-game/src/main/java/dev/steampunkgame/SteamPunkGameApplication.java
@@ -0,0 +1,13 @@
+package dev.steampunkgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamPunkGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamPunkGameApplication.class, args);
+    }
+
+}
