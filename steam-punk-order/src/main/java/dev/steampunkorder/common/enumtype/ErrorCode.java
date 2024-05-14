390d116085ce54624df9cbc23f159fed2c0c10f4
soonhankwon <soonable@naver.com>
2024-05-14 17:41:23 +0900

    feat: 유저 주문ID 미존재 에러코드 추가

--- steam-punk-order/src/main/java/dev/steampunkorder/common/enumtype/ErrorCode.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/common/enumtype/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     NOT_EXISTS_PRODUCT_ID(HttpStatus.BAD_REQUEST, "상품 ID가 존재하지 않습니다."),
     EXISTS_PRODUCT_ID_IN_WISHLIST(HttpStatus.BAD_REQUEST, "상품 ID가 위시리스트에 존재합니다."),
     NOT_EXISTS_PRODUCT_ID_BY_USER(HttpStatus.BAD_REQUEST, "유저 위시리스트에 상품 ID가 존재하지 않습니다."),
-    NOT_EXISTS_ORDER_ID(HttpStatus.BAD_REQUEST, "주문 ID가 존재하지 않습니다.");
+    NOT_EXISTS_ORDER_ID(HttpStatus.BAD_REQUEST, "주문 ID가 존재하지 않습니다."),
+    NOT_EXISTS_ORDER_ID_BY_USER(HttpStatus.BAD_REQUEST, "유저의 해당 주문 ID가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
