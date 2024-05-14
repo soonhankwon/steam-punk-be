d27f0a84a449edbcd9b80732038634a6755fde31
soonhankwon <soonable@naver.com>
2024-05-14 14:28:30 +0900

    feat: 결제(Payment) 마이크로서비스 모듈

---
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/SteamPunkPaymentApplication.java
@@ -0,0 +1,13 @@
+package dev.steampunkpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamPunkPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamPunkPaymentApplication.class, args);
+    }
+
+}
---
+++ steam-punk-payment/src/test/java/dev/steampunkpayment/SteamPunkPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.steampunkpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamPunkPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
