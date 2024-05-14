f88720201adb08bf9d54525c3f4cff450fa54493
soonhankwon <soonable@naver.com>
2024-05-14 17:45:02 +0900

    feat: 결제완료 이벤트 퍼블리셔 - 주문상태를 업데이트, 유저포인트 차감

---
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/event/publish/PaymentCompletedEventPublisher.java
@@ -0,0 +1,49 @@
+package dev.steampunkpayment.event.publish;
+
+import dev.steampunkpayment.domain.OrderInfo;
+import dev.steampunkpayment.dto.request.OrderStateUpdateRequest;
+import dev.steampunkpayment.dto.request.UserPointDecreaseRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class PaymentCompletedEventPublisher {
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void publishPaymentCompletedEvent(PaymentCompletedEvent paymentCompletedEvent) {
+        OrderInfo orderInfo = paymentCompletedEvent.orderInfo();
+        Long orderId = orderInfo.orderId();
+        Long userId = orderInfo.userId();
+        assert orderId != null && userId != null;
+
+        updateOrderState(orderId,
+                OrderStateUpdateRequest.of(userId, "ORDER_PAYMENT_COMPLETED")
+        );
+
+        decreaseUserPoint(userId,
+                UserPointDecreaseRequest.from(orderInfo)
+        );
+    }
+
+    private void updateOrderState(Long orderId, OrderStateUpdateRequest request) {
+        WebClient.create()
+                .patch()
+                .uri("http://localhost:8080/api/v1/orders/" + orderId)
+                .bodyValue(request)
+                .retrieve()
+                .toBodilessEntity()
+                .block();
+    }
+
+    private void decreaseUserPoint(Long userId, UserPointDecreaseRequest request) {
+        WebClient.create()
+                .patch()
+                .uri("http://localhost:8080/api/v1/users/point/" + userId + "/decrease")
+                .bodyValue(request)
+                .retrieve()
+                .toBodilessEntity()
+                .block();
+    }
+}
