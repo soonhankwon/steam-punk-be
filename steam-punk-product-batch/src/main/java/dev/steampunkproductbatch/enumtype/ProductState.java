12adf1d469ba361368ee368e0d6633393c70c558
soonhankwon <soonable@naver.com>
2024-05-16 00:37:48 +0900

    feat: 상품상태(정규, 세일, 한정, 세일한정)

---
+++ steam-punk-product-batch/src/main/java/dev/steampunkproductbatch/enumtype/ProductState.java
@@ -0,0 +1,8 @@
+package dev.steampunkproductbatch.enumtype;
+
+public enum ProductState {
+    ON_SALE_EVENT,
+    ON_SALE_LIMITED_STOCK_EVENT,
+    LIMITED_STOCK_EVENT,
+    REGULAR
+}
---
+++ steam-punk-product/src/main/java/dev/steampunkproduct/domain/ProductState.java
@@ -0,0 +1,14 @@
+package dev.steampunkproduct.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ProductState {
+
+    ON_SALE_EVENT,
+    ON_SALE_LIMITED_STOCK_EVENT,
+    LIMITED_STOCK_EVENT,
+    REGULAR
+}
