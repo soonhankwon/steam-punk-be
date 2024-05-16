691df5a7a48aaceb89b10e52703ab5322edc025d
soonhankwon <soonable@naver.com>
2024-05-16 16:00:35 +0900

    feat: 결제 ID 미존재 에러코드 추가

--- steam-punk-payment/src/main/java/dev/steampunkpayment/common/enumtype/ErrorCode.java
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/common/enumtype/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
 
     INVALID_REQUEST_ORDER_USER_ID(HttpStatus.BAD_REQUEST, "주문의 유저 ID가 요청의 유저 ID와 일치하지 않습니다."),
     NOT_ENOUGH_USER_POINT(HttpStatus.BAD_REQUEST, "유저의 포인트가 충분하지 않습니다."),
-    EXISTS_PAYMENT_HISTORY_BY_ORDER_ID(HttpStatus.BAD_REQUEST, "이미 결제된 주문 ID 입니다.");
+    EXISTS_PAYMENT_HISTORY_BY_ORDER_ID(HttpStatus.BAD_REQUEST, "이미 결제된 주문 ID 입니다."),
+    NOT_EXISTS_PAYMENT_ID(HttpStatus.BAD_REQUEST, "결제 ID가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
