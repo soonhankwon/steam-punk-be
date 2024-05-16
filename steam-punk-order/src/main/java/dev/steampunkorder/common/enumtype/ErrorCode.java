7ed76cccdd38659ef71bd6a0feb67150ca3eaa3c
soonhankwon <soonable@naver.com>
2024-05-16 16:00:24 +0900

    feat: 상품 구매이력 존재 에러코드 추가

--- steam-punk-order/src/main/java/dev/steampunkorder/common/enumtype/ErrorCode.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/common/enumtype/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     EXISTS_PRODUCT_ID_IN_WISHLIST(HttpStatus.BAD_REQUEST, "상품 ID가 위시리스트에 존재합니다."),
     NOT_EXISTS_PRODUCT_ID_BY_USER(HttpStatus.BAD_REQUEST, "유저 위시리스트에 상품 ID가 존재하지 않습니다."),
     NOT_EXISTS_ORDER_ID(HttpStatus.BAD_REQUEST, "주문 ID가 존재하지 않습니다."),
-    NOT_EXISTS_ORDER_ID_BY_USER(HttpStatus.BAD_REQUEST, "유저의 해당 주문 ID가 존재하지 않습니다.");
+    NOT_EXISTS_ORDER_ID_BY_USER(HttpStatus.BAD_REQUEST, "유저의 해당 주문 ID가 존재하지 않습니다."),
+    EXISTS_PAID_HISTORY_PRODUCT(HttpStatus.CONFLICT, "이미 상품을 구매한 이력이 존재합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
