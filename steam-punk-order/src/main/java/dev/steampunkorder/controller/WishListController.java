3030045f24fbc45c1656fe0358967d2da32feeed
soonhankwon <soonable@naver.com>
2024-05-16 16:03:34 +0900

    feat: API path 변경

--- steam-punk-order/src/main/java/dev/steampunkorder/controller/WishListController.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/controller/WishListController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/wishlists")
+@RequestMapping("/api/v1/orders/wishlists")
 public class WishListController {
 
     private final WishListService wishListService;
