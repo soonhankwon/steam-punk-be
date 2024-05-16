68ef13c7b2872557579bc0ca56888b51cdce56e8
soonhankwon <soonable@naver.com>
2024-05-16 21:06:01 +0900

    feat: 상품ID 리스트 컬렉트 메서드 추가 및 유저게임이력 추가 요청에 사용

--- steam-punk-payment/src/main/java/dev/steampunkpayment/event/publish/PaymentCompletedEventPublisher.java
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/event/publish/PaymentCompletedEventPublisher.java
@@ -1,9 +1,12 @@
 package dev.steampunkpayment.event.publish;
 
 import dev.steampunkpayment.domain.OrderInfo;
+import dev.steampunkpayment.domain.OrderProductInfo;
 import dev.steampunkpayment.dto.request.OrderStateUpdateRequest;
 import dev.steampunkpayment.dto.request.UserGameHistoryAddRequest;
 import dev.steampunkpayment.dto.request.UserPointDecreaseRequest;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -19,6 +22,11 @@ public class PaymentCompletedEventPublisher {
         Long userId = orderInfo.userId();
         assert orderId != null && userId != null;
 
+        List<Long> productIds = orderInfo.orderProductInfos()
+                .stream()
+                .map(OrderProductInfo::productId)
+                .collect(Collectors.toList());
+
         updateOrderState(orderId,
                 OrderStateUpdateRequest.of(userId, "ORDER_PAYMENT_COMPLETED")
         );
@@ -28,14 +36,14 @@ public class PaymentCompletedEventPublisher {
         );
 
         addUserGameHistory(
-                UserGameHistoryAddRequest.of(userId, orderInfo.orderProductIds())
+                UserGameHistoryAddRequest.of(userId, productIds)
         );
     }
 
     private void updateOrderState(Long orderId, OrderStateUpdateRequest request) {
         WebClient.create()
                 .patch()
-                .uri("http://localhost:8080/api/v1/orders/" + orderId)
+                .uri("http://localhost:8080/api/v1/orders/{orderId}", orderId)
                 .bodyValue(request)
                 .retrieve()
                 .toBodilessEntity()
@@ -45,7 +53,7 @@ public class PaymentCompletedEventPublisher {
     private void decreaseUserPoint(Long userId, UserPointDecreaseRequest request) {
         WebClient.create()
                 .patch()
-                .uri("http://localhost:8080/api/v1/users/point/" + userId + "/decrease")
+                .uri("http://localhost:8080/api/v1/users/point/{userId}/decrease", userId)
                 .bodyValue(request)
                 .retrieve()
                 .toBodilessEntity()
