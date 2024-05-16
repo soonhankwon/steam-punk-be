447d462af462db708bdc00e6c9ebfe77db0cffe2
soonhankwon <soonable@naver.com>
2024-05-16 21:08:43 +0900

    feat: 결제상태 ENUM

---
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/domain/PaymentState.java
@@ -0,0 +1,9 @@
+package dev.steampunkpayment.domain;
+
+public enum PaymentState {
+    PAYMENT_COMPLETED,
+    PAYMENT_REFUND_IN_PROGRESS,
+    PAYMENT_REFUND_COMPLETED,
+    PAYMENT_PARTIAL_REFUND_IN_PROGRESS,
+    PAYMENT_PARTIAL_REFUND_COMPLETED
+}
