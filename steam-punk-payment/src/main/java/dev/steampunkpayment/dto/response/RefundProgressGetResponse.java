3ffa01a654bbbc77c5ad75d93a911b1ac5f3db60
soonhankwon <soonable@naver.com>
2024-05-16 21:15:12 +0900

    feat: 환불진행상탵 조회 응답 레코드

---
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/dto/response/RefundProgressGetResponse.java
@@ -0,0 +1,32 @@
+package dev.steampunkpayment.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.steampunkpayment.domain.Payment;
+import dev.steampunkpayment.domain.PaymentState;
+import java.time.LocalDateTime;
+
+public record RefundProgressGetResponse(
+        @JsonProperty("payment_id")
+        Long paymentId,
+        @JsonProperty("user_id")
+        Long userId,
+        @JsonProperty("order_id")
+        Long orderId,
+        @JsonProperty("refund_total_price")
+        Long refundTotalPrice,
+        @JsonProperty("refund_progress_at")
+        LocalDateTime refundProgressAt,
+        @JsonProperty("payment_state")
+        PaymentState paymentState
+) {
+    public static RefundProgressGetResponse of(Long refundTotalPrice, Payment payment) {
+        return new RefundProgressGetResponse(
+                payment.getId(),
+                payment.getUserId(),
+                payment.getOrderId(),
+                refundTotalPrice,
+                payment.getModifiedAt(),
+                payment.getPaymentState()
+        );
+    }
+}
