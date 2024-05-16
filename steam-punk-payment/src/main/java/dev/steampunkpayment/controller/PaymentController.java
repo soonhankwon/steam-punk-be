019cb48e5adac4feba8c813ea03c3de62aae4c3d
soonhankwon <soonable@naver.com>
2024-05-16 16:02:44 +0900

    feat: 결제조회 API(GET)

--- steam-punk-payment/src/main/java/dev/steampunkpayment/controller/PaymentController.java
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/controller/PaymentController.java
@@ -2,10 +2,13 @@ package dev.steampunkpayment.controller;
 
 import dev.steampunkpayment.dto.request.PaymentAddRequest;
 import dev.steampunkpayment.dto.response.PaymentAddResponse;
+import dev.steampunkpayment.dto.response.PaymentGetResponse;
 import dev.steampunkpayment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,11 @@ public class PaymentController {
     public ResponseEntity<PaymentAddResponse> addPayment(@RequestBody PaymentAddRequest request) {
         PaymentAddResponse res = paymentService.addPayment(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @GetMapping("{paymentId}")
+    public ResponseEntity<?> getPayment(@PathVariable Long paymentId) {
+        PaymentGetResponse res = paymentService.findPayment(paymentId);
+        return ResponseEntity.ok(res);
     }
 }
