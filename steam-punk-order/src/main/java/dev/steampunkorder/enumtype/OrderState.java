28d6a50c0fa049e38f8bff63d60a4e649791deb3
soonhankwon <soonable@naver.com>
2024-05-16 16:02:31 +0900

    feat: 주문취소 상태 추가

--- steam-punk-order/src/main/java/dev/steampunkorder/enumtype/OrderState.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/enumtype/OrderState.java
@@ -3,5 +3,6 @@ package dev.steampunkorder.enumtype;
 public enum OrderState {
     ORDER_PENDING,
     ORDER_COMPLETED,
-    ORDER_PAYMENT_COMPLETED
+    ORDER_PAYMENT_COMPLETED,
+    ORDER_CANCEL
 }
