41857e3954a41af98bd5f8413462af979acea57b
soonhankwon <soonable@naver.com>
2024-05-16 21:16:12 +0900

    feat: 유저의 게임플레이 이력조회 API(GET)

--- steam-punk-game/src/main/java/dev/steampunkgame/controller/UserGameController.java
+++ steam-punk-game/src/main/java/dev/steampunkgame/controller/UserGameController.java
@@ -3,16 +3,20 @@ package dev.steampunkgame.controller;
 import dev.steampunkgame.dto.request.UserGameAddRequest;
 import dev.steampunkgame.dto.request.UserGamePlayRequest;
 import dev.steampunkgame.dto.response.UserGameAddResponse;
+import dev.steampunkgame.dto.response.UserGameHistoryGetResponse;
 import dev.steampunkgame.dto.response.UserGamePlayResponse;
 import dev.steampunkgame.service.UserGameService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -31,6 +35,13 @@ public class UserGameController {
     @PatchMapping
     public ResponseEntity<UserGamePlayResponse> playGame(@RequestBody UserGamePlayRequest request) {
         UserGamePlayResponse res = userGameService.playGame(request);
+        return ResponseEntity.ok().body(res);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserGameHistoryGetResponse> getUserGameHistory(@PathVariable Long userId,
+                                                                         @RequestParam(value = "productId") Long productId) {
+        UserGameHistoryGetResponse res = userGameService.findUserGameHistory(userId, productId);
         return ResponseEntity.ok().body(res);
     }
 }
