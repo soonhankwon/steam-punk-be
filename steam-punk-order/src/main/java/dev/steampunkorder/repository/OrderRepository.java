c7a3ad48efee6fcde56ffe8d4597ea1acf31ae73
soonhankwon <soonable@naver.com>
2024-05-16 16:01:09 +0900

    feat: 유저ID와 주문상태로 주문조회 쿼리

--- steam-punk-order/src/main/java/dev/steampunkorder/repository/OrderRepository.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package dev.steampunkorder.repository;
 
 import dev.steampunkorder.domain.Order;
+import dev.steampunkorder.enumtype.OrderState;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Optional<Order> findByIdAndUserId(Long orderId, Long userId);
+
+    List<Order> findByUserIdAndOrderState(Long userId, OrderState orderState);
 }
