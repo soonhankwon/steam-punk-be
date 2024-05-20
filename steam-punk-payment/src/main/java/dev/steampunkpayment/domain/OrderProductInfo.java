1642e2859b7e3cce717ba13181b9fb54566a4e63
soonhankwon <soonable@naver.com>
2024-05-20 21:47:27 +0900

    feat: 주문상품상태 추가

--- steam-punk-payment/src/main/java/dev/steampunkpayment/domain/OrderProductInfo.java
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/domain/OrderProductInfo.java
@@ -1,11 +1,14 @@
 package dev.steampunkpayment.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.steampunkpayment.enumtype.OrderProductState;
 
 public record OrderProductInfo(
         @JsonProperty("product_id")
         Long productId,
         @JsonProperty("price")
-        Long price
+        Long price,
+        @JsonProperty("order_product_state")
+        OrderProductState orderProductState
 ) {
 }
