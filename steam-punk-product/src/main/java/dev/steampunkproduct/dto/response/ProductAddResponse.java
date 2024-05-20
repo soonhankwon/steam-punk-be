3c27a5a44ef66b9af9037ffb9cec7e2f8183dcb5
soonhankwon <soonable@naver.com>
2024-05-20 16:37:21 +0900

    feat: 상품 추가 응답 레코드

---
+++ steam-punk-product/src/main/java/dev/steampunkproduct/dto/response/ProductAddResponse.java
@@ -0,0 +1,35 @@
+package dev.steampunkproduct.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.steampunkproduct.domain.Product;
+import dev.steampunkproduct.domain.ProductDiscountPolicy;
+import dev.steampunkproduct.domain.ProductState;
+
+public record ProductAddResponse(
+        String name,
+        Double price,
+        @JsonProperty("short_description")
+        String shortDescription,
+        @JsonProperty("header_image")
+        String headerImage,
+        @JsonProperty("web_site")
+        String webSite,
+        String developer,
+        @JsonProperty("product_state")
+        ProductState productState,
+        @JsonProperty("discount_policy")
+        ProductDiscountPolicy productDiscountPolicy
+) {
+    public static ProductAddResponse from(Product product) {
+        return new ProductAddResponse(
+                product.getName(),
+                product.getPrice(),
+                product.getShortDescription(),
+                product.getHeaderImage(),
+                product.getWebSite(),
+                product.getDeveloper(),
+                product.getProductState(),
+                product.getProductDiscountPolicy()
+        );
+    }
+}
