fc4a906351609f53eb8a0bfb63a259fba7a34340
soonhankwon <soonable@naver.com>
2024-05-20 21:50:56 +0900

    feat: 상품상태 멤버변수 추가

--- steam-punk-order/src/main/java/dev/steampunkorder/domain/ProductInfo.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/domain/ProductInfo.java
@@ -1,6 +1,11 @@
 package dev.steampunkorder.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.steampunkorder.enumtype.OrderProductState;
+
 public record ProductInfo(
-        Long price
+        Long price,
+        @JsonProperty("product_state")
+        OrderProductState productState
 ) {
 }
