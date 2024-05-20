9636d7b620ba03840cb75d76ffd33092e7cb9313
soonhankwon <soonable@naver.com>
2024-05-20 16:37:30 +0900

    feat: 상품 추가 API(POST)

--- steam-punk-product/src/main/java/dev/steampunkproduct/controller/ProductController.java
+++ steam-punk-product/src/main/java/dev/steampunkproduct/controller/ProductController.java
@@ -1,13 +1,18 @@
 package dev.steampunkproduct.controller;
 
+import dev.steampunkproduct.dto.request.ProductAddRequest;
+import dev.steampunkproduct.dto.response.ProductAddResponse;
 import dev.steampunkproduct.dto.response.ProductExistsCheckResponse;
 import dev.steampunkproduct.dto.response.ProductGetResponse;
 import dev.steampunkproduct.service.ProductService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductAddResponse> addProduct(@RequestBody ProductAddRequest request) {
+        ProductAddResponse res = productService.addProduct(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
 
     @GetMapping("{productId}")
     public ResponseEntity<ProductGetResponse> getProduct(@PathVariable Long productId) {
