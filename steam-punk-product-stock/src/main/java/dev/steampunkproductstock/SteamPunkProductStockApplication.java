01f990061dcaf5f813c0ca6d06971f160ee8aa86
soonhankwon <soonable@naver.com>
2024-05-20 15:28:17 +0900

    feat: 실시간 상품 재고 마이크로서비스 모듈

---
+++ steam-punk-product-stock/src/main/java/dev/steampunkproductstock/SteamPunkProductStockApplication.java
@@ -0,0 +1,13 @@
+package dev.steampunkproductstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamPunkProductStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamPunkProductStockApplication.class, args);
+    }
+
+}
---
+++ steam-punk-product-stock/src/test/java/dev/steampunkproductstock/SteamPunkProductStockApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.steampunkproductstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamPunkProductStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
