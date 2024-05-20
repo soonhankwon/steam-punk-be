0739c3c3e9c73dbe94fa6cc2a9ccb8282e9e28d6
soonhankwon <soonable@naver.com>
2024-05-20 18:25:22 +0900

    feat: 결제진입, 결제 비즈니스 로직 분리

--- steam-punk-payment/src/main/java/dev/steampunkpayment/service/PaymentService.java
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/service/PaymentService.java
@@ -3,22 +3,23 @@ package dev.steampunkpayment.service;
 import dev.steampunkpayment.common.enumtype.ErrorCode;
 import dev.steampunkpayment.common.exception.ApiException;
 import dev.steampunkpayment.domain.OrderInfo;
-import dev.steampunkpayment.domain.OrderProductInfo;
 import dev.steampunkpayment.domain.Payment;
-import dev.steampunkpayment.domain.PaymentRefundProduct;
+import dev.steampunkpayment.domain.PaymentProduct;
+import dev.steampunkpayment.domain.PaymentState;
 import dev.steampunkpayment.domain.RefundPolicy;
 import dev.steampunkpayment.domain.UserGamePlayHistoryInfo;
 import dev.steampunkpayment.domain.UserPointInfo;
 import dev.steampunkpayment.dto.request.PaymentAddRequest;
+import dev.steampunkpayment.dto.request.PaymentExecuteRequest;
 import dev.steampunkpayment.dto.response.PaymentAddResponse;
 import dev.steampunkpayment.dto.response.PaymentGetResponse;
 import dev.steampunkpayment.dto.response.RefundProgressAddResponse;
 import dev.steampunkpayment.dto.response.RefundProgressGetResponse;
 import dev.steampunkpayment.event.publish.PaymentCompletedEvent;
-import dev.steampunkpayment.repository.PaymentRefundProductRepository;
+import dev.steampunkpayment.repository.PaymentProductRepository;
 import dev.steampunkpayment.repository.PaymentRepository;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -32,10 +33,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class PaymentService {
 
     private final PaymentRepository paymentRepository;
-    private final PaymentRefundProductRepository paymentRefundProductRepository;
+    private final PaymentProductRepository paymentProductRepository;
     private final ApplicationEventPublisher eventPublisher;
     private final RefundPolicy regularRefundPolicy;
 
+    // 결제 진입 비즈니스 로직: 결제대기 상태 결제 생성
     @Transactional
     public PaymentAddResponse addPayment(PaymentAddRequest request) {
         Long requestOrderId = request.orderId();
@@ -45,14 +47,44 @@ public class PaymentService {
         OrderInfo orderInfo = getOrderInfo(requestOrderId);
         // 유저의 주문인지 밸리데이션
         orderInfo.validate(request);
+        // 결제 대기 상태의 결제 생성 및 INSERT
+        Payment payment = Payment.ofReady(request, orderInfo);
+        payment = paymentRepository.save(payment);
 
+        List<PaymentProduct> paymentProducts = new ArrayList<>();
+        Payment finalPayment = payment;
+        // 결제대기 상태의 결제 상품목록 INSERT
+        orderInfo.orderProductInfos()
+                .forEach(orderProductInfo -> {
+                    PaymentProduct paymentProduct = PaymentProduct.of(finalPayment.getId(),
+                            orderProductInfo.productId(), orderProductInfo.price(),
+                            PaymentState.PAYMENT_READY);
+                    paymentProducts.add(paymentProduct);
+                });
+        paymentProductRepository.saveAll(paymentProducts);
+        return PaymentAddResponse.from(payment);
+    }
+
+    // 결제 비즈니스 로직
+    @Transactional
+    public PaymentAddResponse executePayment(PaymentExecuteRequest request) {
+        Payment payment = paymentRepository.findById(request.paymentId())
+                .orElseThrow(() -> new ApiException(ErrorCode.NOT_EXISTS_PAYMENT_ID));
+
+        // 유저 마이크로 서비스로 유저 포인트 정보 요청
         UserPointInfo userPointInfo = getUserPointInfo(request.userId());
         // 유저가 주문결제에 충분한 포인트를 가지고 있는지 밸리데이션
-        userPointInfo.validatePoint(orderInfo);
+        userPointInfo.validatePoint(payment.getTotalPrice());
+        OrderInfo orderInfo = getOrderInfo(payment.getOrderId());
+        Long paymentId = payment.getId();
 
-        Payment payment = Payment.of(request, orderInfo);
-        payment = paymentRepository.save(payment);
-        eventPublisher.publishEvent(PaymentCompletedEvent.from(orderInfo));
+        List<PaymentProduct> paymentProducts = paymentProductRepository.findAllByPaymentIdAndPaymentState(
+                paymentId, PaymentState.PAYMENT_READY);
+        // 결제대기인 상품들을 완료상태로 변경 - Dirty Check
+        paymentProducts.forEach(PaymentProduct::paid);
+        payment.complete();
+
+        eventPublisher.publishEvent(PaymentCompletedEvent.from(payment, orderInfo.orderProductInfos()));
         return PaymentAddResponse.from(payment);
     }
 
@@ -81,34 +113,36 @@ public class PaymentService {
         return PaymentGetResponse.from(payment);
     }
 
+    // 환불 진행 API
     @Transactional
     public RefundProgressAddResponse addRefundInProgress(Long paymentId) {
         Payment payment = paymentRepository.findById(paymentId)
                 .orElseThrow(() -> new ApiException(ErrorCode.NOT_EXISTS_PAYMENT_ID));
 
-        // 결제 주문정보를 조회함
-        OrderInfo paidOrderInfo = getOrderInfo(payment.getOrderId());
-        // 결제 완료된 주문의 상품들에 대한 정보 MAP (Key: PaymentId, Value: Price - 당시 가격(스냅샷))
-        Map<Long, Long> paidOrderProductInfoMap = paidOrderInfo.orderProductInfos()
-                .stream()
-                .collect(Collectors.toMap(OrderProductInfo::productId, OrderProductInfo::price));
+        // 결제완료 상태인 결제 상품목록을 조회
+        List<PaymentProduct> paymentProducts = paymentProductRepository.findAllByPaymentIdAndPaymentState(
+                payment.getId(),
+                PaymentState.PAYMENT_COMPLETED
+        );
 
         // 결제완료된 주문 상품들에 대한 플레이 기록을 가져와서 리스트로 변환
         Long userId = payment.getUserId();
-        List<UserGamePlayHistoryInfo> userGamePlayHistoryInfos = paidOrderInfo.orderProductInfos()
+        List<UserGamePlayHistoryInfo> userGamePlayHistoryInfos = paymentProducts
                 .stream()
-                .map(orderProductInfo -> getUserGamePlayHistory(userId, orderProductInfo.productId()))
+                .map(paymentProduct -> getUserGamePlayHistory(userId, paymentProduct.getProductId()))
                 .toList();
 
-        // 정규 환불 정책 적용 - 환불 조건에 충족되는 상품만 결제 환불 상품에 등록(환불 IN PROCESS)
+        // 정규 환불 정책 적용 - 환불 조건에 충족되는 상품만 결제 환불 진행 상태로 업데이트(환불 IN PROCESS)
         AtomicReference<Long> refundTotalPrice = new AtomicReference<>(0L);
         userGamePlayHistoryInfos.stream()
                 .filter(i -> i.hasRefundCondition(regularRefundPolicy))
                 .forEach(i -> {
-                            Long refundPrice = paidOrderProductInfoMap.get(i.productId());
+                            PaymentProduct paymentProduct = paymentProductRepository.findByPaymentIdAndProductId(paymentId,
+                                            i.productId())
+                                    .orElseThrow(() -> new ApiException(ErrorCode.NOT_EXISTS_PAYMENT_ID));
+                            paymentProduct.refundInProgress();
+                            Long refundPrice = paymentProduct.getPrice();
                             refundTotalPrice.updateAndGet(v -> v + refundPrice);
-                            paymentRefundProductRepository.save(
-                                    PaymentRefundProduct.of(paymentId, i, refundPrice));
                         }
                 );
 
@@ -135,10 +169,13 @@ public class PaymentService {
     public RefundProgressGetResponse findRefundInProgress(Long paymentId) {
         Payment payment = paymentRepository.findById(paymentId)
                 .orElseThrow(() -> new ApiException(ErrorCode.NOT_EXISTS_PAYMENT_ID));
-        List<PaymentRefundProduct> paymentRefundProducts = paymentRefundProductRepository.findAllByPaymentId(paymentId);
+        List<PaymentProduct> paymentRefundProducts = paymentProductRepository.findAllByPaymentIdAndPaymentState(
+                paymentId,
+                PaymentState.PAYMENT_REFUND_IN_PROGRESS
+        );
 
         long totalRefundPrice = paymentRefundProducts.stream()
-                .mapToLong(PaymentRefundProduct::getPrice)
+                .mapToLong(PaymentProduct::getPrice)
                 .sum();
 
         return RefundProgressGetResponse.of(totalRefundPrice, payment);
