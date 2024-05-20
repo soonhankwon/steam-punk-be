c5b7ae1ee15a0112220871f315d8cb02a44aaf5f
soonhankwon <soonable@naver.com>
2024-05-20 15:47:39 +0900

    feat: 상품재고 트랜잭션 서비스

---
+++ steam-punk-product-stock/src/main/java/dev/steampunkproductstock/service/StockTransactionService.java
@@ -0,0 +1,28 @@
+package dev.steampunkproductstock.service;
+
+import dev.steampunkproductstock.common.enumtype.ErrorCode;
+import dev.steampunkproductstock.common.exception.ApiException;
+import dev.steampunkproductstock.domain.ProductStock;
+import dev.steampunkproductstock.repository.ProductStockRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class StockTransactionService {
+
+    private final ProductStockRepository productStockRepository;
+
+    @Transactional
+    public ProductStock updateByTransaction(Long productId) {
+        // 비관적락 적용 - 쓰기 락
+        ProductStock productStock = productStockRepository.findByProductId(productId)
+                .orElseThrow(() -> new ApiException(ErrorCode.NOT_EXIST_PRODUCT_ID));
+        productStock.decreaseStock();
+        log.info("realStock={}", productStock.getStockQuantity());
+        return productStock;
+    }
+}
