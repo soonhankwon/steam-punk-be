0735d4e9e64bba38d36b6c9006cf12125c19fd27
soonhankwon <soonable@naver.com>
2024-05-20 18:26:06 +0900

    feat: 메서드 파라미터 변경(범용성 개선)

--- steam-punk-payment/src/main/java/dev/steampunkpayment/dto/request/UserPointDecreaseRequest.java
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/dto/request/UserPointDecreaseRequest.java
@@ -1,11 +1,9 @@
 package dev.steampunkpayment.dto.request;
 
-import dev.steampunkpayment.domain.OrderInfo;
-
 public record UserPointDecreaseRequest(
         Long point
 ) {
-    public static UserPointDecreaseRequest from(OrderInfo orderInfo) {
-        return new UserPointDecreaseRequest(orderInfo.totalPrice());
+    public static UserPointDecreaseRequest from(Long paidTotalPrice) {
+        return new UserPointDecreaseRequest(paidTotalPrice);
     }
 }
