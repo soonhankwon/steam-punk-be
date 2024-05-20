eb18b2e1f6c46a72fc7f6642889af943714308c2
soonhankwon <soonable@naver.com>
2024-05-20 15:45:27 +0900

    feat: 상품재고 엔티티 정의

---
+++ steam-punk-product-stock/src/main/java/dev/steampunkproductstock/domain/ProductStock.java
@@ -0,0 +1,54 @@
+package dev.steampunkproductstock.domain;
+
+import dev.steampunkproductstock.common.entity.BaseTimeEntity;
+import dev.steampunkproductstock.common.enumtype.ErrorCode;
+import dev.steampunkproductstock.common.exception.ApiException;
+import dev.steampunkproductstock.dto.request.ProductStockAddRequest;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "product_stock")
+public class ProductStock extends BaseTimeEntity {
+
+    private static final long MIN_STOCK_QUANTITY = 0L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_stock_id")
+    private Long id;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "stock_quantity")
+    private Long stockQuantity;
+
+    private ProductStock(Long productId, Long stockQuantity) {
+        this.productId = productId;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public static ProductStock from(ProductStockAddRequest request) {
+        return new ProductStock(
+                request.productId(),
+                request.stockQuantity()
+        );
+    }
+
+    public void decreaseStock() {
+        if (this.stockQuantity == MIN_STOCK_QUANTITY) {
+            throw new ApiException(ErrorCode.MINIMUM_STOCK_REACHED);
+        }
+        this.stockQuantity--;
+    }
+}
