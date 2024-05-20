5c29316a539e0575689eaf73a228101a186bf569
soonhankwon <soonable@naver.com>
2024-05-20 18:21:35 +0900

    feat: 결제상품 레포지토리

---
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/repository/PaymentProductRepository.java
@@ -0,0 +1,15 @@
+package dev.steampunkpayment.repository;
+
+import dev.steampunkpayment.domain.PaymentProduct;
+import dev.steampunkpayment.domain.PaymentState;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentProductRepository extends JpaRepository<PaymentProduct, Long> {
+    List<PaymentProduct> findAllByPaymentIdAndPaymentState(Long id, PaymentState paymentState);
+
+    Optional<PaymentProduct> findByPaymentIdAndProductId(Long paymentId, Long productId);
+}
