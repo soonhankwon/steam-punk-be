9695ca48955158134b6d62988ccdd036fb95c7f9
soonhankwon <soonable@naver.com>
2024-05-20 16:37:49 +0900

    feat: 상품 조회시 상품상태, 할인정책 멤버변수 추가

--- steam-punk-product/src/main/java/dev/steampunkproduct/dto/response/ProductGetResponse.java
+++ steam-punk-product/src/main/java/dev/steampunkproduct/dto/response/ProductGetResponse.java
@@ -2,6 +2,8 @@ package dev.steampunkproduct.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.steampunkproduct.domain.Product;
+import dev.steampunkproduct.domain.ProductDiscountPolicy;
+import dev.steampunkproduct.domain.ProductState;
 
 public record ProductGetResponse(
         @JsonProperty("product_id")
@@ -13,7 +15,11 @@ public record ProductGetResponse(
         String headerImage,
         @JsonProperty("web_site")
         String webSite,
-        String developer
+        String developer,
+        @JsonProperty("product_state")
+        ProductState productState,
+        @JsonProperty("discount_policy")
+        ProductDiscountPolicy discountPolicy
 
 ) {
     public static ProductGetResponse from(Product product) {
@@ -24,7 +30,9 @@ public record ProductGetResponse(
                 product.getShortDescription(),
                 product.getHeaderImage(),
                 product.getWebSite(),
-                product.getDeveloper()
+                product.getDeveloper(),
+                product.getProductState(),
+                product.getProductDiscountPolicy()
         );
     }
 }
