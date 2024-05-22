7c0fa5b914dd41757a53210fcd985b94ccf34781
soonhankwon <soonable@naver.com>
2024-05-22 20:14:56 +0900

    feat: 카테고리 엔티티 추가

---
+++ steam-punk-product/src/main/java/dev/steampunkproduct/domain/Category.java
@@ -0,0 +1,26 @@
+package dev.steampunkproduct.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+}
