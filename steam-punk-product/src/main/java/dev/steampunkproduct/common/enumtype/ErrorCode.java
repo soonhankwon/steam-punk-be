7fa5f1cfbe20f54835db0433cced13afdcfd1625
soonhankwon <soonable@naver.com>
2024-05-22 20:15:13 +0900

    feat: 카테고리 ID 미존재 에러코드 추가

--- steam-punk-product/src/main/java/dev/steampunkproduct/common/enumtype/ErrorCode.java
+++ steam-punk-product/src/main/java/dev/steampunkproduct/common/enumtype/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    NOT_EXIST_PRODUCT_ID(HttpStatus.BAD_REQUEST, "상품 ID가 존재하지 않습니다.");
+    NOT_EXISTS_PRODUCT_ID(HttpStatus.BAD_REQUEST, "상품 ID가 존재하지 않습니다."),
+    NOT_EXISTS_CATEGORY_ID(HttpStatus.BAD_REQUEST, "카테고리 ID가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
