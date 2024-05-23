76b2bbae89cb0ea70d1fdb5346585a59c579411f
soonhankwon <soonable@naver.com>
2024-05-23 20:59:38 +0900

    refactor: json 응답 변경, 불필요 멤버변수 삭제

--- steam-punk-user/src/main/java/dev/steampunkuser/dto/response/UserPointUpdateResponse.java
+++ steam-punk-user/src/main/java/dev/steampunkuser/dto/response/UserPointUpdateResponse.java
@@ -2,17 +2,14 @@ package dev.steampunkuser.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.steampunkuser.domain.User;
-import java.time.LocalDateTime;
 
 public record UserPointUpdateResponse(
-        Long point,
-        @JsonProperty("updated_at")
-        LocalDateTime updatedAt
+        @JsonProperty("current_point")
+        Long point
 ) {
     public static UserPointUpdateResponse from(User user) {
         return new UserPointUpdateResponse(
-                user.getPoint(),
-                LocalDateTime.now()
+                user.getPoint()
         );
     }
 }
