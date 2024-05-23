fa0fbe140a8adbd3c995bb4088ee2b7ce75029d0
soonhankwon <soonable@naver.com>
2024-05-23 22:36:43 +0900

    feat: 불필요 멤버변수 삭제

--- steam-punk-order/src/main/java/dev/steampunkorder/dto/response/OrderUpdateResponse.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/dto/response/OrderUpdateResponse.java
@@ -3,18 +3,14 @@ package dev.steampunkorder.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.steampunkorder.domain.Order;
 import dev.steampunkorder.enumtype.OrderState;
-import java.time.LocalDateTime;
 
 public record OrderUpdateResponse(
         @JsonProperty("order_state")
-        OrderState orderState,
-        @JsonProperty("updated_at")
-        LocalDateTime updated_at
+        OrderState orderState
 ) {
     public static OrderUpdateResponse from(Order order) {
         return new OrderUpdateResponse(
-                order.getOrderState(),
-                order.getModifiedAt()
+                order.getOrderState()
         );
     }
 }
