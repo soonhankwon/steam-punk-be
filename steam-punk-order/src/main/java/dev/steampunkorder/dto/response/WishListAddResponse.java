fce4c6f5efea07573a6a2bbdb87e527f1cc4a9b2
soonhankwon <soonable@naver.com>
2024-05-23 22:44:44 +0900

    feat: 불필요 멤버변수 삭제

--- steam-punk-order/src/main/java/dev/steampunkorder/dto/response/WishListAddResponse.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/dto/response/WishListAddResponse.java
@@ -2,7 +2,6 @@ package dev.steampunkorder.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.steampunkorder.domain.WishList;
-import java.time.LocalDateTime;
 
 public record WishListAddResponse(
         @JsonProperty("wish_list_id")
@@ -10,16 +9,13 @@ public record WishListAddResponse(
         @JsonProperty("user_id")
         Long userId,
         @JsonProperty("product_id")
-        Long productId,
-        @JsonProperty("created_at")
-        LocalDateTime createdAt
+        Long productId
 ) {
     public static WishListAddResponse from(WishList wishList) {
         return new WishListAddResponse(
                 wishList.getId(),
                 wishList.getUserId(),
-                wishList.getProductId(),
-                wishList.getCreatedAt()
+                wishList.getProductId()
         );
     }
 }
