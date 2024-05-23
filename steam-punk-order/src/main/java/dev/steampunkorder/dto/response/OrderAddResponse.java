bef823ec92cd8462bb21a9b81d0fe38981081b35
soonhankwon <soonable@naver.com>
2024-05-23 22:13:23 +0900

    refactor: 불필요 API 스펙 멤버변수 삭제

--- steam-punk-order/src/main/java/dev/steampunkorder/dto/response/OrderAddResponse.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/dto/response/OrderAddResponse.java
@@ -3,18 +3,14 @@ package dev.steampunkorder.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.steampunkorder.domain.Order;
 import dev.steampunkorder.enumtype.OrderState;
-import java.time.LocalDateTime;
 
 public record OrderAddResponse(
         @JsonProperty("order_state")
-        OrderState orderState,
-        @JsonProperty("created_at")
-        LocalDateTime createdAt
+        OrderState orderState
 ) {
     public static OrderAddResponse from(Order order) {
         return new OrderAddResponse(
-                order.getOrderState(),
-                order.getCreatedAt()
+                order.getOrderState()
         );
     }
 }
