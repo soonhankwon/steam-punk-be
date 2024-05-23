ba250f0e2467967576caf79f568d0b3877a4ca05
soonhankwon <soonable@naver.com>
2024-05-23 17:49:25 +0900

    feat: 카테고리 이름 검색

--- steam-punk-product/src/main/java/dev/steampunkproduct/repository/CategoryRepository.java
+++ steam-punk-product/src/main/java/dev/steampunkproduct/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package dev.steampunkproduct.repository;
 
 import dev.steampunkproduct.domain.Category;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
