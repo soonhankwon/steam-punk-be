7fdb3ef3d1ed589230d209714d4e110152f928c0
soonhankwon <soonable@naver.com>
2024-05-24 18:50:16 +0900

    feat: 게임추가 응답 API 스펙변경 로직적용

--- steam-punk-game/src/main/java/dev/steampunkgame/service/UserGameService.java
+++ steam-punk-game/src/main/java/dev/steampunkgame/service/UserGameService.java
@@ -3,6 +3,7 @@ package dev.steampunkgame.service;
 import dev.steampunkgame.common.enumtype.ErrorCode;
 import dev.steampunkgame.common.exception.ApiException;
 import dev.steampunkgame.domain.UserGameHistory;
+import dev.steampunkgame.dto.GameDTO;
 import dev.steampunkgame.dto.request.UserGameAddRequest;
 import dev.steampunkgame.dto.request.UserGamePlayRequest;
 import dev.steampunkgame.dto.response.UserGameAddResponse;
@@ -11,7 +12,6 @@ import dev.steampunkgame.dto.response.UserGamePlayResponse;
 import dev.steampunkgame.repository.UserGameHistoryRepository;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,19 +24,22 @@ public class UserGameService {
 
     // 유저의 게임 주문 결제 후 호출될 비즈니스 로직: 게임을 플레이하지 않은 기본 이력으로 만들어짐
     @Transactional
-    public List<UserGameAddResponse> addUserGame(UserGameAddRequest request) {
+    public UserGameAddResponse addUserGame(UserGameAddRequest request) {
         List<UserGameHistory> userGameHistories = new ArrayList<>();
+        Long userId = request.userId();
         request.productIds().forEach(id -> {
-            if (userGameHistoryRepository.existsByUserIdAndProductId(request.userId(), id)) {
+            if (userGameHistoryRepository.existsByUserIdAndProductId(userId, id)) {
                 throw new ApiException(ErrorCode.EXISTS_USER_GAME_HISTORY);
             }
             UserGameHistory userGameHistory = UserGameHistory.ofNotPlayedState(id, request);
             userGameHistories.add(userGameHistory);
         });
         userGameHistoryRepository.saveAll(userGameHistories);
-        return userGameHistories.stream()
-                .map(UserGameAddResponse::from)
-                .collect(Collectors.toList());
+
+        List<GameDTO> gameDTOS = userGameHistories.stream()
+                .map(GameDTO::from)
+                .toList();
+        return UserGameAddResponse.of(userId, gameDTOS);
     }
 
     @Transactional
