552f42a20a16cad124d7732fc01f8e30418104cc
soonhankwon <soonable@naver.com>
2024-05-24 23:13:50 +0900

    feat: 생성일시, 수정일시 컬럼추가

--- steam-punk-product-batch/src/main/java/dev/steampunkproductbatch/domain/Product.java
+++ steam-punk-product-batch/src/main/java/dev/steampunkproductbatch/domain/Product.java
@@ -12,11 +12,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -28,17 +31,33 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
     private Long id;
+
     private String name;
+
     private Double price;
+
     @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
     private String headerImage;
+
     private String webSite;
+
     private String developer;
+
     @Enumerated(EnumType.STRING)
     private DiscountPolicy discountPolicy;
+
     @Enumerated(EnumType.STRING)
     private ProductState productState;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
     private final List<ProductCategory> productCategories = new ArrayList<>();
@@ -53,5 +72,7 @@ public class Product {
         this.developer = developer;
         this.discountPolicy = DiscountPolicy.REGULAR;
         this.productState = ProductState.REGULAR;
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
     }
 }
