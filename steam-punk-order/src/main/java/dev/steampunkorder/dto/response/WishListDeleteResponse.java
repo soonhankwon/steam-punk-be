5b8b4d41ebaceda7f57227293bf3f91c9f0a4ba6
soonhankwon <soonable@naver.com>
2024-05-24 16:50:21 +0900

    refactor: Boolean 박싱

--- steam-punk-order/src/main/java/dev/steampunkorder/dto/response/WishListDeleteResponse.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/dto/response/WishListDeleteResponse.java
@@ -1,18 +1,14 @@
 package dev.steampunkorder.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.time.LocalDateTime;
 
 public record WishListDeleteResponse(
         @JsonProperty("is_deleted")
-        boolean isDeleted,
-        @JsonProperty("deleted_at")
-        LocalDateTime deletedAt
+        Boolean isDeleted
 ) {
     public static WishListDeleteResponse ofSuccess() {
         return new WishListDeleteResponse(
-                true,
-                LocalDateTime.now()
+                Boolean.TRUE
         );
     }
 }
