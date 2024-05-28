732e25c4dcb5308ddcac2cd71996f1875f8e9895
soonhankwon <soonable@naver.com>
2024-05-28 13:05:58 +0900

    feat: 위시리스트에 상품추가시 결제완료된 주문의 상품 추가 예외처리

--- steam-punk-order/src/main/java/dev/steampunkorder/service/WishListService.java
+++ steam-punk-order/src/main/java/dev/steampunkorder/service/WishListService.java
@@ -2,6 +2,7 @@ package dev.steampunkorder.service;
 
 import dev.steampunkorder.common.enumtype.ErrorCode;
 import dev.steampunkorder.common.exception.ApiException;
+import dev.steampunkorder.domain.OrderProduct;
 import dev.steampunkorder.domain.WishList;
 import dev.steampunkorder.dto.WishListDTO;
 import dev.steampunkorder.dto.request.WishListAddRequest;
@@ -10,6 +11,9 @@ import dev.steampunkorder.dto.response.ProductsExistsCheckResponse;
 import dev.steampunkorder.dto.response.WishListAddResponse;
 import dev.steampunkorder.dto.response.WishListDeleteResponse;
 import dev.steampunkorder.dto.response.WishListGetResponse;
+import dev.steampunkorder.enumtype.OrderState;
+import dev.steampunkorder.repository.OrderProductRepository;
+import dev.steampunkorder.repository.OrderRepository;
 import dev.steampunkorder.repository.WishListRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +26,28 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WishListService {
 
     private final WishListRepository wishListRepository;
+    private final OrderRepository orderRepository;
+    private final OrderProductRepository orderProductRepository;
 
     @Transactional
     public WishListAddResponse addWishList(WishListAddRequest request) {
-        if (wishListRepository.existsByUserIdAndProductId(request.userId(), request.productId())) {
+        Long userId = request.userId();
+        // 동일한 상품을 위시리스트에 담을 수 없음
+        if (wishListRepository.existsByUserIdAndProductId(userId, request.productId())) {
             throw new ApiException(ErrorCode.EXISTS_PRODUCT_ID_IN_WISHLIST);
         }
+        // 위시리스트에 이미 결제완료된 주문의 상품을 담을 수 없음 (예외처리)
+        Long productId = request.productId();
+        boolean isExistPaidProduct = orderRepository.findByUserIdAndOrderState(userId, OrderState.ORDER_PAID)
+                .stream()
+                .flatMap(o -> orderProductRepository.findAllByOrderId(o.getId()).stream()
+                )
+                .map(OrderProduct::getProductId)
+                .anyMatch(id -> id.equals(productId));
+        if (isExistPaidProduct) {
+            throw new ApiException(ErrorCode.EXISTS_PAID_HISTORY_PRODUCT);
+        }
+
         ProductsExistsCheckResponse productsExistsCheckResponse = checkProductExists(request);
         if (!productsExistsCheckResponse.isExists()) {
             throw new ApiException(ErrorCode.NOT_EXISTS_PRODUCT_ID);
