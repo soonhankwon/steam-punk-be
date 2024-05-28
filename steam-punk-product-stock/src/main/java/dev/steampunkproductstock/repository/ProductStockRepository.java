49ff71e119385325918c230e8b65adbee2e9b5ff
soonhankwon <soonable@naver.com>
2024-05-28 18:31:20 +0900

    feat: 비관적락 적용 읽기쿼리, 일반 읽기 쿼리 분리

--- steam-punk-product-stock/src/main/java/dev/steampunkproductstock/repository/ProductStockRepository.java
+++ steam-punk-product-stock/src/main/java/dev/steampunkproductstock/repository/ProductStockRepository.java
@@ -5,10 +5,14 @@ import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductStockRepository extends JpaRepository<ProductStock, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select ps from ProductStock ps WHERE :productId = ps.productId")
+    Optional<ProductStock> findByProductIdWithLock(Long productId);
+
     Optional<ProductStock> findByProductId(Long productId);
 }
