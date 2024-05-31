451048a62c80ca73caa750b597b9428853929740
soonhankwon <soonable@naver.com>
2024-05-31 12:24:51 +0900

    feat: 내부 주문서비스 API를 사용해서 주문정보 객체를 생성하는 정적팩터리 메서드

--- steam-punk-payment/src/main/java/dev/steampunkpayment/domain/OrderInfo.java
+++ steam-punk-payment/src/main/java/dev/steampunkpayment/domain/OrderInfo.java
@@ -5,6 +5,7 @@ import dev.steampunkpayment.common.enumtype.ErrorCode;
 import dev.steampunkpayment.common.exception.ApiException;
 import dev.steampunkpayment.dto.request.PaymentAddRequest;
 import java.util.List;
+import org.springframework.web.reactive.function.client.WebClient;
 
 public record OrderInfo(
         @JsonProperty("meta")
@@ -30,5 +31,14 @@ public record OrderInfo(
 
     private boolean isRequestUserIdEqualsOrderInfoUserId(PaymentAddRequest request) {
         return request.userId().equals(this.orderMetaData.userId());
+    }
+
+    public static OrderInfo fromOrderInfoInternalApi(Long orderId) {
+        return WebClient.create()
+                .get()
+                .uri("http://localhost:8080/api/v1/orders/{orderId}", orderId)
+                .retrieve()
+                .bodyToMono(OrderInfo.class)
+                .block();
     }
 }
